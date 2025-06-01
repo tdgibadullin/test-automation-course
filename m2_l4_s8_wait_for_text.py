6b8531453fdc7e929bbcbd2e68952aed613684f6
tdgibadullin <tdgibadullin@gmail.com>
2025-06-01 23:41:51 +0300

    Add explicit wait script and update README.md

---
+++ m2_l4_s8_wait_for_text.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+    )
+
+    button = browser.find_element(By.ID, "book")
+    button.click()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input_field = browser.find_element(By.ID, "answer")
+    input_field.send_keys(y)
+
+    button = browser.find_element(By.ID, "solve")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
